8bbbde908e4a701eb5f7fa3c03be33bba48fde4f
Jefferson <legacyofspirits167@gmail.com>
2025-09-07 20:20:38 -0400

    Cambiada la bd a postgresql con sv de prueba en render

--- api.py
+++ api.py
@@ -2,53 +2,60 @@ from fastapi import FastAPI, Depends, HTTPException, status, Form
 from pydantic import BaseModel
 import sqlite3
 import os
+import psycopg2
 
 app = FastAPI()
 
 class Message(BaseModel):
     text: str
 
-def get_db():
-    # Establece la conexión a la base de datos
-    db = sqlite3.connect("messages.db")
-    # Crea la tabla si no existe
-    cur = db.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS mensajes (id INTEGER PRIMARY KEY, text TEXT)")
-    db.commit()
-    # Retorna la conexión
+def connect_db():
+    db = psycopg2.connect("postgresql://example_1ula_user:TS3mlURmO9E3FAnIUWjSugX4nWqwfVKc@dpg-d2tfclnfte5s73a6kbq0-a.oregon-postgres.render.com/example_1ula")
+    print("Conectado exitosamente!")
     return db
+    
+
+# def get_db():
+#     # Establece la conexión a la base de datos
+#     db = sqlite3.connect("messages.db")
+#     # Crea la tabla si no existe
+#     cur = db.cursor()
+#     cur.execute("CREATE TABLE IF NOT EXISTS mensajes (id INTEGER PRIMARY KEY, text TEXT)")
+#     db.commit()
+#     # Retorna la conexión
+#     return db
 
 @app.get("/")
 async def root():
     return {"message": "Hello this is API for create Messages"}
 
-@app.get("/messages")
+@app.get("/all-messages")
 async def get_messages():
-    db = get_db()
+    db = connect_db()
     cur = db.cursor()
-    cur.execute("SELECT id, text FROM mensajes")
+    cur.execute("SELECT * FROM mensajes")
     mensajes = cur.fetchall()
     db.close()
     return [{"id": mensaje[0], "text": mensaje[1]} for mensaje in mensajes]
 
 @app.post("/message")
 async def create_message(mensaje: Message):
-    db = get_db()
+    db = connect_db()
     cursor = db.cursor()
-    cursor.execute("INSERT INTO mensajes (text) VALUES (?)", (mensaje.text, ))
+    cursor.execute("INSERT INTO mensajes (mensaje_contenido) VALUES (%s) RETURNING mensaje_id", (mensaje.text, ))
+    mensaje_id = cursor.fetchone()[0]
     db.commit()
-    mensaje_id = cursor.lastrowid
     db.close()
     return {"id": mensaje_id, "text": mensaje.text}
 
 #Endpoint para formularios html ( de prueba )
 @app.post("/messages-form")
 async def create_message_with_Form(text: str = Form(...)):
-    db = get_db()
+    db = connect_db()
     cursor = db.cursor()
-    cursor.execute("INSERT INTO mensajes (text) VALUES (?)", (text, ))
+    cursor.execute("INSERT INTO mensajes (mensaje_contenido) VALUES (%s) RETURNING mensaje_id", (text, ))
+    mensaje_id = cursor.fetchone()[0]
     db.commit()
-    mensaje_id = cursor.lastrowid
     db.close()
     return {"id": mensaje_id, "text": text}
 
